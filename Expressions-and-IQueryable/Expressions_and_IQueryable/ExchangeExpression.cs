c1d326421527e89b4c1ab85db40632ddebebd14b
Artsem Zhaldak <Artsem_Zhaldak@epam.com>
2018-10-14 23:41:54 +0300

    first task

---
+++ Expressions-and-IQueryable/Expressions_and_IQueryable/ExchangeExpression.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressions_and_IQueryable
+{
+	public class ExchangeExpression : ExpressionVisitor
+	{
+		private Dictionary<string, int> _exchangeDict;
+
+		public ExchangeExpression()
+		{
+
+		}
+
+		public ExchangeExpression(Dictionary<string, int> exchangeDict)
+		{
+			_exchangeDict = exchangeDict;
+		}
+		private int depth = 0;
+
+		public override Expression Visit(Expression node)
+		{
+			depth++;
+			var result = base.Visit(node);
+			depth--;
+
+			return result;
+		}
+		protected override Expression VisitBinary(BinaryExpression node)
+		{
+			//ParameterExpression parameterLeft = null;
+			//ParameterExpression parameterRight = null;
+			//ConstantExpression constant = null;
+
+			//if (node.Right.NodeType == ExpressionType.Parameter && node.Left.NodeType == ExpressionType.Parameter)
+			//{
+			//	parameterLeft = (ParameterExpression)node.Left;
+			//	parameterRight = (ParameterExpression)node.Right;
+
+			//	if (_exchangeDict.ContainsKey(parameterRight.Name) && _exchangeDict.ContainsKey(parameterLeft.Name))
+			//	{
+			//		return Expression.Constant(_exchangeDict[parameterRight.Name] + _exchangeDict[parameterLeft.Name]);
+			//	}
+			//}
+
+
+
+
+			//if (node.Right.NodeType == ExpressionType.Parameter)
+			//{
+			//	parameterRight = (ParameterExpression)node.Right;
+			//	if (_exchangeDict.ContainsKey(parameterRight.Name))
+			//	{
+			//		return Expression.Constant(_exchangeDict[parameterRight.Name]);
+			//	}
+			//}
+
+			//if (node.Left.NodeType == ExpressionType.Parameter)
+			//{
+			//	parameterLeft = (ParameterExpression)node.Left;
+			//	if (_exchangeDict.ContainsKey(parameterLeft.Name))
+			//	{
+			//		return Expression.Constant(_exchangeDict[parameterLeft.Name]);
+			//	}
+			//}
+
+			return base.VisitBinary(node);
+		}
+
+		protected override Expression VisitParameter(ParameterExpression node)
+		{
+			if (depth > 2)
+			{
+				if (_exchangeDict.ContainsKey(node.Name))
+				{
+					return Expression.Constant(_exchangeDict[node.Name]);
+				}
+			}
+
+			return base.VisitParameter(node);
+		}
+	}
+}
---
+++ Expressions-and-IQueryable/Expressions_and_IQueryable/ExpressionVisitorTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Expressions_and_IQueryable
+{
+	[TestClass]
+	public class ExpressionVisitorTest
+	{
+		// Task1 - Замена выражений вида<переменная> + 1 / <переменная> - 1 на операции инкремента и декремента
+	   [TestMethod]
+		public void IncremenAndtDecrement()
+		{
+			Expression<Func<int, int, int>> source = (a, b) => (a + 1) / (b - 1);
+
+			var result = new CustomExpressionTransformator().VisitAndConvert(source, "");
+
+			Console.WriteLine(source); // (a, b) => ((a + 1) / (b - 1))
+			Console.WriteLine(result); // (a, b) => (Increment(a) / Decrement(b))
+
+			Assert.AreEqual(source.Compile().Invoke(9, 6), result.Compile().Invoke(9, 6));
+		}
+
+		// Task2 - Замену параметров, входящих в lambda-выражение, на константы
+		[TestMethod]
+		public void ExchangeParams()
+		{
+			Dictionary<string, int> dict = new Dictionary<string, int>()
+			{
+				{ "a", 2 },
+				{ "b", 4 },
+				{ "c", 6 }
+			};
+
+			Expression<Func<int, int, int, int>> source = (a, b, c) => a + b + c;
+
+			var result = new ExchangeExpression(dict).VisitAndConvert(source, "");
+
+			Console.WriteLine(result); // (a, b, c) => ((2 + 4) + 6)
+		}
+
+	}
+}
